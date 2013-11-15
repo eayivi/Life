f53ac45e3cfc8c421294a213e2335af3b2dcc80e
eayivi <enam.ayivi@utexas.edu>
2013-11-14 18:28:54 -0600

    added a couple of abstract classes

--- AbstractCell.h
+++ AbstractCell.h
@@ -1,1 +1,23 @@
- 
+// --------------------------
+// projects/life/AbstractCell.h
+// Copyright (C) 2013
+// Glenn P. Downing
+// --------------------------
+
+// --------
+// includes
+// --------
+
+#include <iostream> // istream, ostream
+
+class AbstractCell {
+
+  private:
+    bool state;
+    int age;
+
+  public:
+    virtual bool turn() ;  // could check neighbors and follow rules   
+}
+
+    
--- ConwayCell.h
+++ ConwayCell.h
@@ -1,1 +1,20 @@
+// --------------------------
+// projects/life/AbstractCell.h
+// Copyright (C) 2013
+// Glenn P. Downing
+// --------------------------
+
+// --------
+// includes
+// --------
+
+#include <iostream> // istream, ostream
+
+class Cell  {
+
+  private:
+    AbstractCell* pAbstractCell;
+
+}
+
  
