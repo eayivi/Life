a16277bdebddf6f46c8817e10db9d924cf31e1f4
Cristhian Escobar <cje466@cs.utexas.edu>
2013-11-21 20:42:19 -0600

    Life.h documentation setup

--- Life.h
+++ Life.h
@@ -15,26 +15,47 @@
 #include "AbstractCell.h"
 #include "Cell.h"
 
+// ------
+// Class Life 
+// ------
 
+/**
+  * Class Life template <typename T>
+  */
 template <typename T>
 class Life{
 
     private:
-      int row_size; 
-      int col_size;
-      int generation;
-      int population;  
+      int row_size; /**< # of rows in the grid */
+      int col_size; /**< # of columns in the grid */
+      int generation; /**< current generation */
+      int population;  /**< current population */
     
-      vector< vector<T> > board; 
+      vector< vector<T> > board; /**< Grid of T objects*/
 
 
     public: 
+        /** 
+          * Default Life costructor
+          */
         Life();
 
+         /** 
+          *@param Number of rows
+          *@param Number of columns 
+          *@param Generation 
+          *@param Population
+          */
         Life (int num_row, int num_col) : row_size(num_row), col_size (num_col), generation(0), population(0) {
             board.resize(num_row+2, vector<T>(num_col+2));
         }
 
+         // -----------
+        // giveEveryoneOneTurn
+        // -----------
+        /** 
+          *  Generate a boar to simulate Life from a text file
+          */
         void replicateBoard(istream& in){
             char currentCell;
             for(int i = 1; i < row_size+1; i++){                 
@@ -45,12 +66,16 @@ class Life{
                     board[i][j].cometolife();
                     population ++;
                   }
-                }
-                
+                }   
             }
-    
        }
 
+        // -----------
+        // count_neighbors
+        // -----------
+        /** 
+          * Update number count of neighbors in the current generation
+          */
        void count_neighbors () {
             //cout << endl << "counting neighbors..." << endl;
             for(int i = 1; i < row_size+1; i++){ 
@@ -81,11 +106,15 @@ class Life{
                     count++;
                   board[i][j].set_diag_neighbors (count);                                    
                 }
-
             }        
-
        }
 
+        // -----------
+        // printBoard
+        // -----------
+        /** 
+          * Print board of the current generation
+          */
         void printBoard (std::ostream& w) {
             w << "Generation = " << generation ;
             w << ", Population = " << population << endl;
@@ -105,19 +134,30 @@ class Life{
             }
        }
 
+        // -----------
+        // printneighbor_count
+        // -----------
+        /** 
+          * Print count of neighbors
+          */
        void printneighbor_count (std::ostream& w) {
             for (int i = 0; i < row_size+2; ++i){
                 for (int j= 0; j <col_size+2 ; ++j) {
-
                     if (board[i][j].neighbors_cnt != 0)
                       cout << " position " << i << "," << j << " has " << board[i][j].neighbors_cnt << " neighbors" <<endl ;
                 }
             }
        }
 
+        // -----------
+        // simulateNth
+        // -----------
+        /**  Simulate # of life generations 
+          * @param number of moves to simulate 
+          * @param number of board simulations to print 
+          */
        void simulateNth(int num_moves, int nbr_print =0 ) {
-
-          
+  
           for (int k = 0; k < num_moves; k++) {
             count_neighbors();
             if (nbr_print > 0)
@@ -135,10 +175,6 @@ class Life{
             generation++;
           }
 
-       }
-
- };
-
-
+       }};
 
  #endif // Life_h
